580c1e184d119e3bb71381676f6c430d1f6915d8
MrTimscampi <julien.machiels@protonmail.com>
2020-11-26 08:39:36 +0100

    feat(login): add public users

--- pages/login.vue
+++ pages/login.vue
@@ -2,8 +2,10 @@
   <v-container fill-height>
     <v-row align="center" justify="center">
       <v-col md="4">
-        <h1 class="text-h4 mb-6 text-center">{{ $t('login') }}</h1>
-        <login-form />
+        <div v-if="currentUser || loginAsOther || !publicUsers">
+          <h1 class="text-h4 mb-6 text-center">{{ $t('login') }}</h1>
+          <login-form />
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -12,10 +14,18 @@
 <script lang="ts">
 import Vue from 'vue';
 import { mapActions } from 'vuex';
+import { UserDto } from '~/api';
 
 export default Vue.extend({
   layout: 'fullpage',
   middleware: 'serverMiddleware',
+  data() {
+    return {
+      loginAsOther: false,
+      currentUser: {} as UserDto,
+      publicUsers: [] as Array<UserDto>
+    };
+  },
   head() {
     return {
       title: this.$store.state.page.title
@@ -23,6 +33,13 @@ export default Vue.extend({
   },
   created() {
     this.setPageTitle({ title: this.$t('login') });
+  },
+  async beforeMount() {
+    try {
+      this.publicUsers = (await this.$api.user.getPublicUsers({})).data;
+    } catch (error) {
+      console.error('Unable to get public users:', error);
+    }
   },
   methods: {
     ...mapActions('page', ['setPageTitle'])
